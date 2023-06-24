e190c8450c0f924e1a5fecd1a8b88a67fb464c73
YJ Soon <yjsoon@tinkertanker.com>
2023-06-24 09:35:16 +0800

    Initial Commit

---
+++ GitHub Demo App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitHub Demo App
+//
+//  Created by YJ Soon on 24/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHub Demo App/GitHub_Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_Demo_AppApp.swift
+//  GitHub Demo App
+//
+//  Created by YJ Soon on 24/6/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
